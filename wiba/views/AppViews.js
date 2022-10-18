d2e6b9c3fba15f6d129519e56231f364f1bd675e
nanbooray <raytengxz@gmail.com>
2022-10-18 21:23:46 +0800

    Added other general functions for frontend

--- wiba/views/AppViews.js
+++ wiba/views/AppViews.js
@@ -53,5 +53,28 @@ exports.fundAccount = class extends React.Component {
   }
 }
 
+exports.DeployerOrAttacher = class extends React.Component {
+  render() {
+    const {parent} = this.props;
+    return (
+      <div>
+        Please select a role:
+        <br />
+        <p>
+          <button
+            onClick={() => parent.selectDeployer()}
+          >Deployer</button>
+          <br /> Set the wager, deploy the contract.
+        </p>
+        <p>
+          <button
+            onClick={() => parent.selectAttacher()}
+          >Attacher</button>
+          <br /> Attach to the Deployer's contract.
+        </p>
+      </div>
+    );
+  }
+}
 
 export default exports;
